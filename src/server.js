469b3bf202d5d664cd9541a5f89d3bfaaa8df56e
rishabhmthakur2 <rishabhmthakur@outlook.com>
2020-06-28 02:15:52 +0530

    uphold button auth connection and redirect

--- src/server.js
+++ src/server.js
@@ -7,6 +7,12 @@ const publicPath = '../public'
 
 app.use(express.static(path.join(__dirname, publicPath)));
 
+app.get('/loggedIn',(req, res)=>{
+    const returnCode = req.params.code;
+    console.log(returnCode);
+    res.send('You are logged in now');
+});
+
 app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server listing on port: ${process.env.PORT || PORT}`);
 });
