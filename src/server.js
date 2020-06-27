ffdceb18c5ec42554f0687720f00d0231efda3e2
rishabhmthakur2 <rishabhmthakur@outlook.com>
2020-06-28 02:44:27 +0530

    Making auth request for token

--- src/server.js
+++ src/server.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const path = require('path');
+const request = require('request');
 
 const app = express();
 const PORT = 3000;
 const publicPath = '../public'
+const returnCode = "";
+const client_id = "f56ec8dd7cc31ab9f199a3c599e879aa27dfcb63";
+const client_secret = "e3849adec6208c914ab461b9ed9c2d04cbbfcf7c";
+
 
 app.use(express.static(path.join(__dirname, publicPath)));
 
-app.get('/loggedIn',(req, res)=>{
-    const returnCode = req.query.code;
-    res.send(returnCode);
+app.get('/loggedIn', async (req, res) => {
+    returnCode = req.query.code;
+    await request.post({url:`https://api.uphold.com/oauth2/token`,form: {
+        client_id: client_id,
+        client_secret: client_secret,
+        code: returnCode,
+        grant_type: 'authorization_code'
+    }},async (err, response)=>{
+        if(err){
+            return res.send(err);
+        }
+        res.send(response);
+    })
 });
 
-app.listen(process.env.PORT || PORT, ()=>{
+app.listen(process.env.PORT || PORT, () => {
     console.log(`Server listing on port: ${process.env.PORT || PORT}`);
 });
 
