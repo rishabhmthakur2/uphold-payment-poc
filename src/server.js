0c64cb0d2c3c2bc9067082d6ae20d6863df4e2f8
rishabhmthakur2 <rishabhmthakur@outlook.com>
2020-06-29 23:22:03 +0530

    Parsed response to JSON for Cards and getCardsById - Stringify and using typeof

--- src/server.js
+++ src/server.js
@@ -10,7 +10,6 @@ const client_id = "f56ec8dd7cc31ab9f199a3c599e879aa27dfcb63";
 const client_secret = "e3849adec6208c914ab461b9ed9c2d04cbbfcf7c";
 var authToken = ""
 
-
 app.use(express.static(path.join(__dirname, publicPath)));
 
 app.get('/loggedIn', async (req, res) => {
@@ -36,7 +35,7 @@ app.get('/getCards', async (req, res) => {
             'Authorization': `Bearer ${authToken}`
         },
     }, (err, response) => {
-        res.send(JSON.parse(response.body));
+        res.send(typeof(response.body) + '\n' + JSON.parse(response.body));
     });
 });
 
@@ -47,10 +46,9 @@ app.get('/getCards/:id', async (req, res) => {
             'Authorization': `Bearer ${authToken}`
         },
     }, (err, response) => {
-        res.send(JSON.parse(response.body));
+        res.send(typeof(response.body) + '\n' + JSON.stringify(response.body));
     });
 });
-
 
 app.post('/createCard', async (req, res) => {
     const cardObject = {
@@ -68,7 +66,6 @@ app.post('/createCard', async (req, res) => {
         res.send(`New card created: ${res.body.id}`);
     });
 });
-
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server listing on port: ${process.env.PORT || PORT}`);
